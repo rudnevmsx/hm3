580f239b651bafaca39d7791da91c26b3361381b
rudnevmsx <rudnevmsx@gmail.com>
2023-10-25 20:52:55 +0300

    init

--- src/main/java/ru/otus/rudnev/basic/homework3/Main.java
+++ src/main/java/ru/otus/rudnev/basic/homework3/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         sumOfPositiveElements(new int[][]{{1,2,3},{1,3,4}});
         secondTask(4);
-        thirdTask(new int[][]{{1,2,3},{1,3,4},{1,8,3}});
+        thirdTask(new int[][]{{1,2},{1,3},{1,8}});
         findMax(new int[][]{{7,8,4},{4,7,2},{16,1,2}});
         fifthTask(new int[][]{{1,2,3},{4,5,6},{8,3,7}});
     }
@@ -30,12 +30,19 @@ public class Main {
             System.out.println();
         }
     }
-
+    //и что будет если пришлют прямоугольный массив?
     public static void thirdTask(int [][] arr){
         int size = arr.length;
-        for(int i = 0; i < size; i++){
-            arr[i][size-1-i] = 0;
+
+        for (int i = 0; i < size ; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (i == j){
+                    arr[i][j] = 0;
+                }
+            }
+
         }
+
         //Вывод готового массива
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[0].length; j++) {
@@ -44,12 +51,12 @@ public class Main {
             System.out.println();
         }
     }
-
+    //а что будет если пришлют массив с одними отрицательными числами?
     public static void findMax(int [][] arr){
         int maxValue = 0;
         for(int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr[i].length; j++) {
-                if(arr[i][j] > maxValue){
+                if(Math.abs(arr[i][j]) > Math.abs(maxValue)){
                     maxValue = arr[i][j];
                 }
             }
@@ -57,14 +64,15 @@ public class Main {
         System.out.println("Максимальный элемент массива: " + maxValue);
     }
 
-    public static void fifthTask(int [][] arr){
+    //вернуть != распечатать
+    public static int fifthTask(int [][] arr){
         int sum = 0;
         if (arr.length < 2){
-            System.out.println("-1");
+            return -1;
         }
         for (int j = 0; j < arr[1].length; j++) {
             sum += arr[1][j];
         }
-        System.out.println("Сумма элементов второй строки: " + sum);
+        return sum;
     }
 }
